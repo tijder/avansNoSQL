a459e9c4f6c9cddfeeea7728a13f68bb83946125
Gerben Droogers <gerben@droogers.eu>
2018-11-28 16:49:57 +0100

    User schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required.']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required.']
+  }
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
