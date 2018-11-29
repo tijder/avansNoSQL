284c0c2901e52b6500cc15317507c6e738ea5702
Gerben Droogers <gerben@droogers.eu>
2018-11-29 12:31:37 +0100

    Remove ; in tests

--- test/users_test.js
+++ test/users_test.js
@@ -4,96 +4,91 @@ const server = require('../app')
 const should = chai.should()
 const expect = chai.expect()
 
-let token = "";
-let testId
-
 chai.use(chaiHttp)
 describe('Users API interface', () => {
-    var username = "Test " + Math.floor((Math.random() * 1000000000000) + 1);
+    var username = "UserTest " + Math.floor((Math.random() * 1000000000000) + 1)
     var password = "test"
     it('should POST /users incorrect if params missing', done => {
       chai.request(server)
         .post('/users')
         .end((err, res) => {
-          res.should.have.status(400);
-          done();
-        });
-    });
+          res.should.have.status(400)
+          done()
+        })
+    })
     it('should POST /users correct', done => {
         chai.request(server)
           .post('/users')
           .send({name:username,password:password})
           .end((err, res) => {
-            res.should.have.status(200);
+            res.should.have.status(200)
             res.body.name.should.equal(username)
             res.body.password.should.equal(password)
             res.body.active.should.equal(true)
-
-            testId = res.body.id
-            done();
-          });
-      });
+            done()
+          })
+      })
       it('should POST /users incorrect if allready exist', done => {
         chai.request(server)
           .post('/users')
           .send({name:username,password:password})
           .end((err, res) => {
-            res.should.have.status(422);
-            done();
-          });
-      });
+            res.should.have.status(422)
+            done()
+          })
+      })
       it('should PUT /users incorrect if params missing', done => {
         chai.request(server)
           .put('/users')
           .end((err, res) => {
-            res.should.have.status(400);
-            done();
-          });
-      });
+            res.should.have.status(400)
+            done()
+          })
+      })
       it('should PUT /users correct', done => {
         chai.request(server)
           .put('/users')
           .send({name:username,password:password,newpassword:password + 'new'})
           .end((err, res) => {
-            res.should.have.status(200);
+            res.should.have.status(200)
             res.body.password.should.equal(password + 'new')
-            done();
-          });
-      });
+            done()
+          })
+      })
       it('should PUT /users incorrect if password is incorrect', done => {
         chai.request(server)
           .put('/users')
           .send({name:username,password:'incorrect',newpassword:password + 'new'})
           .end((err, res) => {
-            res.should.have.status(401);
-            done();
-          });
-      });
+            res.should.have.status(401)
+            done()
+          })
+      })
       it('should DELETE /users incorrect if params missing', done => {
         chai.request(server)
           .delete('/users')
           .end((err, res) => {
-            res.should.have.status(400);
-            done();
-          });
-      });
+            res.should.have.status(400)
+            done()
+          })
+      })
       it('should DELETE /users correct', done => {
         chai.request(server)
           .delete('/users')
           .send({name:username,password:password + 'new'})
           .end((err, res) => {
-            res.should.have.status(200);
+            res.should.have.status(200)
             res.body.active.should.equal(false)
-            done();
-          });
-      });
+            done()
+          })
+      })
       it('should DELETE /users incorrect if password is incorrect', done => {
         chai.request(server)
           .delete('/users')
           .send({name:username,password:'incorrect',newpassword:password + 'new'})
           .end((err, res) => {
-            res.should.have.status(401);
-            done();
-          });
-      });
+            res.should.have.status(401)
+            done()
+          })
+      })
 })
