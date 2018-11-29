edcce24e5dabf7f689c5b972c2b3c010b65b4955
Gerben Droogers <gerben@droogers.eu>
2018-11-29 13:34:21 +0100

    Fix post comment

--- controllers/ctrl_comments.js
+++ controllers/ctrl_comments.js
@@ -1,154 +1,166 @@
-const Thread = require('../models/thread');
-const Comment = require('../models/comment');
-const User = require('../models/user');
+const Thread = require('../models/thread')
+const Comment = require('../models/comment')
+const User = require('../models/user')
 
 module.exports = {
-  get(req, res, next) {},
+  get(req, res, next) {
+    Thread.find({}, function (err, threads) {
+      res.send(threads)
+    })
+  },
 
   create(req, res, next) {
-    if (req.body['content'] === undefined || req.body['userId'] === undefined) {
-      console.log('ERROR 400', req.body);
+    if (req.body['content'] === undefined || req.body['userName'] === undefined) {
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
 
-    if(req.body['threadId'] !== undefined) {
+    if(req.params.threadid !== undefined) {
       Thread.findOne({
-        _id: req.body['threadId']
+        _id: req.params.threadid
       }, function (err, thread) {
         if (err) {
-          res.status(400).json(err);
+          res.status(400).json(err)
+        } else if (!thread) {
+          res.status(422).json({})
         } else {
           User.findOne({
-            _id: req.body['userId']
+            name: req.body['userName']
           }, function (err, user) {
             if (err) {
-              res.status(400).json(err);           
+              res.status(400).json(err)           
+            } else if (!user) {
+              res.status(422).json({})
             } else {
               let comment = new Comment({
                 content: req.body['content'],
-                user: req.body['userId']
-              });
+                user: user.id
+              })
   
               comment.save()
                 .then(() => {
-                  thread.comments = thread.comments.concat(comment);
+                  thread.comments = thread.comments.concat(comment)
                   thread.save(function (err) {
                     if (err) {
-                      res.status(400).json(err);           
+                      res.status(400).json(err)           
                     } else {
-                      comment.username = user.name;
-                      res.status(200).json(comment);           
+                      comment.username = user.name
+                      res.status(200).json(comment)           
                     }
-                  });
+                  })
                 }).catch(err => {
-                  res.status(400).json(err);
-                });
+                  res.status(400).json(err)
+                })
             }
-          });
+          })
         }
-      });  
+      })  
     }
-    if(req.body['commentId'] !== undefined) {
+    if(req.params.commentid !== undefined) {
       Comment.findOne({
-        _id: req.body['commentId']
+        _id: req.params.commentid
       }, function (err, comment) {
         if (err) {
-          res.status(400).json(err);           
+          res.status(400).json(err)           
+        } else if (!comment) {
+          res.status(422).json({})
         } else {
           User.findOne({
-            _id: req.body['userId']
+            _id: req.body['userName']
           }, function (err, user) {
             if (err) {
-              res.status(400).json(err);        
+              res.status(400).json(err)        
+            } else if (!user) {
+              res.status(422).json({})
             } else {
               let newComment = new Comment({
                 content: req.body['content'],
-                user: req.body['userId']
-              });
+                user: user.id
+              })
   
               newComment.save()
                 .then(() => {
-                  comment.comments = comment.comments.concat(comment);
+                  comment.comments = comment.comments.concat(comment)
                   comment.save(function (err) {
                     if (err) {
-                      res.status(400).json(err);
+                      res.status(400).json(err)
                     } else {
-                      comment.username = user.name;
-                      res.status(200).json(comment);           
+                      comment.username = user.name
+                      res.status(200).json(comment)           
                     }
-                  });
+                  })
                 }).catch(err => {
-                  res.status(400).json(err);
-                });
+                  res.status(400).json(err)
+                })
             }
-          });
+          })
         }
-      });
+      })
     }
   },
 
   createUpvote(req, res, next) {
     if (req.body['commentId'] === undefined || req.body['userId'] === undefined) {
-      console.log('ERROR 400', req.body);
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
 
-    let comment;
+    let comment
 
     Comment.findOne({
         _id: req.body['commentId']
       })
       .then((result) => {
-        comment = result;
+        comment = result
       })
       .then(() => User.findOne({
         _id: req.body['userId']
       }))
       .then((user) => {
         const voted = comment.votes.filter(function (element) {
-          return element.user.toString() === user._id.toString();
-        });
+          return element.user.toString() === user._id.toString()
+        })
 
         if (voted.length === 0) {
           comment.votes = comment.votes.concat({
             rated: true,
             user: user._id
-          });
+          })
         } else if (voted[0].rated === false) {
-          let doc = comment.votes.id(voted[0]._id);
+          let doc = comment.votes.id(voted[0]._id)
           doc.set({
             rated: true
-          });
+          })
         }
 
         comment.save(function (err) {
           if (err) {
-            res.status(400).json(err);
+            res.status(400).json(err)
           } else {
             res.status(200).json({
               message: 'Upvoted comment: ' + req.body['commentId']
-            });
+            })
           }
-        });
+        })
       })
       .catch(err => {
-        res.status(400).json(err);
-      });
+        res.status(400).json(err)
+      })
   },
 
   destroy(req, res, next) {
     if (req.body['id'] === undefined) {
-      console.log('ERROR 400', req.body);
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
 
     Comment.update({
@@ -160,156 +172,156 @@ module.exports = {
       }
     }, function (err) {
       if (err) {
-        res.status(400).json(err);
+        res.status(400).json(err)
       } else {
         res.status(200).json({
           message: 'Deleted comment: ' + req.body['id']
-        });
+        })
       }
-    });
+    })
   },
 
   createDownvote(req, res, next) {
     if (req.body['commentId'] === undefined || req.body['userId'] === undefined) {
-      console.log('ERROR 400', req.body);
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
 
-    let comment;
+    let comment
 
     Comment.findOne({
         _id: req.body['commentId']
       })
       .then((result) => {
-        comment = result;
+        comment = result
       })
       .then(() => User.findOne({
         _id: req.body['userId']
       }))
       .then((user) => {
         const voted = comment.votes.filter(function (element) {
-          return element.user.toString() === user._id.toString();
-        });
+          return element.user.toString() === user._id.toString()
+        })
 
         if (voted.length === 0) {
           comment.votes = comment.votes.concat({
             rated: false,
             user: user._id
-          });
+          })
         } else if (voted[0].rated === true) {
-          let doc = comment.votes.id(voted[0]._id);
+          let doc = comment.votes.id(voted[0]._id)
           doc.set({
             rated: false
-          });
+          })
         }
 
         comment.save(function (err) {
           if (err) {
-            res.status(400).json(err);
+            res.status(400).json(err)
           } else {
             res.status(200).json({
               message: 'Upvoted comment: ' + req.body['commentId']
-            });
+            })
           }
-        });
+        })
       })
       .catch(err => {
-        res.status(400).json(err);
-      });
+        res.status(400).json(err)
+      })
   },
 
   destroyUpvote(req, res, next) {
     if (req.body['commentId'] === undefined || req.body['userId'] === undefined) {
-      console.log('ERROR 400', req.body);
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
     
-    let comment;
+    let comment
 
     Comment.findOne({
         _id: req.body['commentId']
       })
       .then((result) => {
-        comment = result;
+        comment = result
       })
       .then(() => User.findOne({
         _id: req.body['userId']
       }))
       .then((user) => {
-        comment.username = user.name;
+        comment.username = user.name
 
         const voted = comment.votes.filter(function (element) {
-          return element.user.toString() === user._id.toString();
-        });
+          return element.user.toString() === user._id.toString()
+        })
 
         if (voted.length === 1 && voted[0].rated === true) {
-          comment.votes.id(voted[0]._id).remove();
+          comment.votes.id(voted[0]._id).remove()
         }
 
         comment.save(function (err) {
           if (err) {
-            res.status(400).json(err);         
+            res.status(400).json(err)         
           } else {
             res.status(200).json({
               message: 'Deleted upvote from comment: ' + req.body['commentId']
-            });
+            })
           }
-        });
+        })
       })
       .catch(err => {
-        res.status(400).json(err);
-      });
+        res.status(400).json(err)
+      })
   },
 
   destroyDownvote(req, res, next) {
     if (req.body['commentId'] === undefined || req.body['userId'] === undefined) {
-      console.log('ERROR 400', req.body);
+      console.log('ERROR 400', req.body)
       res.status(400).json({
         message: 'Missing or wrong parameters.'
-      });
-      return;
+      })
+      return
     }
     
-    let comment;
+    let comment
 
     Comment.findOne({
         _id: req.body['commentId']
       })
       .then((result) => {
-        comment = result;
+        comment = result
       })
       .then(() => User.findOne({
         _id: req.body['userId']
       }))
       .then((user) => {
-        comment.username = user.name;
+        comment.username = user.name
 
         const voted = comment.votes.filter(function (element) {
-          return element.user.toString() === user._id.toString();
-        });
+          return element.user.toString() === user._id.toString()
+        })
 
         if (voted.length === 1 && voted[0].rated === false) {
-          comment.votes.id(voted[0]._id).remove();
+          comment.votes.id(voted[0]._id).remove()
         }
 
         comment.save(function (err) {
           if (err) {
-            res.status(400).json(err);          
+            res.status(400).json(err)          
           } else {
             res.status(200).json({
               message: 'Deleted upvote from comment: ' + req.body['commentId']
-            });
+            })
           }
-        });
+        })
       })
       .catch(err => {
-        res.status(400).json(err);
-      });
+        res.status(400).json(err)
+      })
   }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -3,15 +3,17 @@ var router = express.Router();
 
 const commentController = require('../controllers/ctrl_comments')
 
-router.post('thread/:threadid', commentController.create) // 4a
-router.post('comment/:commentid', commentController.create) // 4b
+router.get('/', commentController.get) // 4a
 
-router.delete(':commentid', commentController.destroy) // 4a
+router.post('/threads/:threadid', commentController.create) // 4a
+router.post('/comments/:commentid', commentController.create) // 4b
 
-router.post(':commentid/upvote', commentController.createUpvote) // 4d
-router.delete(':commentid/upvote', commentController.destroyUpvote) // 4d
+router.delete('/:commentid', commentController.destroy) // 4a
 
-router.post(':commentid/downvote', commentController.createDownvote) // 4e
-router.delete(':commentid/downvote', commentController.destroyDownvote) // 4e
+router.post('/:commentid/upvote', commentController.createUpvote) // 4d
+router.delete('/:commentid/upvote', commentController.destroyUpvote) // 4d
+
+router.post('/:commentid/downvote', commentController.createDownvote) // 4e
+router.delete('/:commentid/downvote', commentController.destroyDownvote) // 4e
 
 module.exports = router;
