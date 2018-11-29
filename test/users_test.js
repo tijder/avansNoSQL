b4592245817674e2c6dad0ced86acbc0654aab43
Gerben Droogers <gerben@droogers.eu>
2018-11-29 11:52:08 +0100

    Created test for post /user

---
+++ test/users_test.js
@@ -0,0 +1,45 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../app')
+const should = chai.should()
+const expect = chai.expect()
+
+let token = "";
+let testId
+
+chai.use(chaiHttp)
+describe('Users API interface', () => {
+    var username = "Test " + Math.floor((Math.random() * 1000000000000) + 1);
+    var password = "test"
+    it('should POST /users incorrect if params missing', done => {
+      chai.request(server)
+        .post('/users')
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it('should POST /users correct', done => {
+        chai.request(server)
+          .post('/users')
+          .send({name:username,password:password})
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.name.should.equal(username)
+            res.body.password.should.equal(password)
+            res.body.active.should.equal(true)
+
+            testId = res.body.id
+            done();
+          });
+      });
+      it('should POST /users incorrect if allready exist', done => {
+        chai.request(server)
+          .post('/users')
+          .send({name:username,password:password})
+          .end((err, res) => {
+            res.should.have.status(422);
+            done();
+          });
+      });
+})
